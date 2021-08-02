3054ffa2d1dca3abb26416cf963b15e9c1cfb7af
KimberlyTdrm <83626561+KimberlyTdrm@users.noreply.github.com>
2021-08-02 18:10:30 -0400

    Updated index.js file

---
+++ index.js
@@ -0,0 +1,128 @@
+function formatTime(time) {
+        let hours = now.getHours();
+        if (hours < 10) {
+          hours = `0${hours}`;
+        }
+        let minutes = now.getMinutes();
+        if (minutes < 10) {
+          minutes = `0${minutes}`;
+        }
+        return `${hours}:${minutes}`;
+      }
+
+      let now = new Date();
+
+      let date = now.getDate();
+
+      let year = now.getFullYear();
+      let days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+      ];
+      let day = days[now.getDay()];
+      let months = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "June",
+        "July",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      let month = months[now.getMonth()];
+
+      let currentTime = new Date();
+      currentTime = document.querySelector("span#current-time");
+
+      currentTime.innerHTML = formatTime(currentTime);
+
+      let todaysDate = document.querySelector("#todays-date");
+      todaysDate.innerHTML = `${month} ${date}, ${year}`;
+      let currentDay = document.querySelector("#current-day");
+      currentDay.innerHTML = `${day}`;
+
+      function displayWeatherEnvironment(response) {
+        console.log(response);
+        document.querySelector("#current-city").innerHTML = response.data.name;
+        document.querySelector("#current-temperature").innerHTML = Math.round(
+          response.data.main.temp
+        );
+        document.querySelector("#current-description").innerHTML =
+          response.data.weather[0].description;
+        document.querySelector("#feels-like").innerHTML = Math.round(
+          response.data.main.feels_like
+        );
+        document.querySelector("#wind").innerHTML = Math.round(
+          response.data.wind.speed
+        );
+        document.querySelector("#humidity").innerHTML =
+          response.data.main.humidity;
+      }
+
+      function searchCity(city) {
+        let apiKey = "0bcd7ddcb27e38fd12ad8e86572870d2";
+        let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        let units = "imperial";
+        let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+        axios.get(apiUrl).then(displayWeatherEnvironment);
+      }
+
+      function exploreMetropolis(event) {
+        event.preventDefault();
+        let city = document.querySelector("#city-input").value;
+        searchCity(city);
+      }
+
+      let searchForm = document.querySelector("#search-city-form");
+      searchForm.addEventListener("submit", exploreMetropolis);
+
+      searchCity("New York");
+
+      function showCurrentWeather(response) {
+        console.log(response.data);
+        document.querySelector("#current-city").innerHTML = response.data.name;
+        document.querySelector("#current-temperature").innerHTML = Math.round(
+          response.data.main.temp
+        );
+        document.querySelector("#current-description").innerHTML =
+          response.data.weather[0].description;
+        document.querySelector("#feels-like").innerHTML = Math.round(
+          response.data.main.feels_like
+        );
+        document.querySelector("#wind").innerHTML = Math.round(
+          response.data.wind.speed
+        );
+        document.querySelector("#humidity").innerHTML =
+          response.data.main.humidity;
+      }
+
+      function fetchPosition(position) {
+        console.log(position);
+        let apiKey = "0bcd7ddcb27e38fd12ad8e86572870d2";
+        let latitude = position.coords.latitude;
+        let longitude = position.coords.longitude;
+        let units = "imperial";
+        let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+        console.log(apiUrl);
+        axios.get(apiUrl).then(showCurrentWeather);
+      }
+
+      function getGps(event) {
+        event.preventDefault();
+        navigator.geolocation.getCurrentPosition(fetchPosition);
+      }
+
+      let currentLocation = document.querySelector("#current-location-button");
+      currentLocation.addEventListener("click", getGps);
+  
